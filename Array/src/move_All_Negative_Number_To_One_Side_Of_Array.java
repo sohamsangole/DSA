ee0a5c84fcf17b6517d212bbf275a71c07e03995
Soham Sangole <sohamajaysangole@gmail.com>
2023-06-15 17:40:38 +0530

    Solved 4 array questions

--- Array/src/move_All_Negative_Number_To_One_Side_Of_Array.java
+++ Array/src/move_All_Negative_Number_To_One_Side_Of_Array.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class move_All_Negative_Number_To_One_Side_Of_Array {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class move_All_Negative_Number_To_One_Side_Of_Array {
+    /*
+    0 1 2 3 4 5 6
+    length = 7
+    2 -> 4
+    length - 1 - i
+     */
+    public static int[] separateNegativeAndPositive(int[] a) {
+        Arrays.sort(a);
+        return a;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,-3,4,-5,6,7,-8,-9};
+        System.out.println(Arrays.toString(arr));
+        separateNegativeAndPositive(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
